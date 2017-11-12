37494f32f0daedad822315a652d029025b37429f
Bouteloup <remy.bouteloup@yahoo.fr>
2017-11-12 17:15:30 -0500

    Initialisation de la borne

--- src/model/Borne.java
+++ src/model/Borne.java
@@ -22,14 +22,27 @@ public class Borne
      */
     public Borne(int id, int nombreVelosMax, int nombreVelosDispo, ArrayList<Socle> listSocle)
     {
-        this.id = id;
-        this.nombreVelosMax = nombreVelosMax;
-        this.nombreVelosDispo = nombreVelosDispo;
-        this.listSocle = listSocle;
+        this.id = 72;
+        this.nombreVelosMax = 5;
+        this.nombreVelosDispo = 5;
+        this.listSocle = new ArrayList<Socle>();
+
+        // Initialisation de 5 socles contenant un vélo chacun
+        listSocle.add(0, new Socle(1, true, true, new Velo(1, true, true)));
+        listSocle.add(1, new Socle(2, true, true, new Velo(2, true, true)));
+        listSocle.add(2, new Socle(3, true, true, new Velo(3, true, true)));
+        listSocle.add(3, new Socle(4, true, true, new Velo(4, true, true)));
+        listSocle.add(4, new Socle(5, true, true, new Velo(5, true, true)));
     }
 
+    /**
+     * Vérifie si un vélo est disponible
+     * 
+     * @return
+     */
     public Velo verifierVelo()
     {
-        return 0;
+        // RETURN A SUPPRIMER
+        return new Velo(0, true, true);
     }
 }
