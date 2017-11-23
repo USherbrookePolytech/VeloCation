1246ccdea5589958a1efc7f34e563b824fee2bae
Bouteloup <remy.bouteloup@yahoo.fr>
2017-11-23 15:52:05 -0500

    Mise a jour vue louer

--- src/view/AccueilView.java
+++ src/view/AccueilView.java
@@ -72,108 +72,98 @@ public class AccueilView extends AbstractView
 
         JLabel AccueilLblVelocation = new JLabel("Bienvenue !");
         AccueilLblVelocation.setFont(new Font("Tahoma", Font.BOLD, 25));
-        
+
         JLabel AccueilLblBorne = new JLabel("Vous \u00EAtes sur la borne n°72");
         AccueilLblBorne.setFont(new Font("Tahoma", Font.BOLD, 14));
 
         JLabel AccueilLblBienvenue = new JLabel("5 rue de l'Interface - J1H Q4H Sherbrooke");
         AccueilLblBienvenue.setFont(new Font("Tahoma", Font.ITALIC, 13));
-        
+
         JButton AccueilBtnAide = AbstractView.btnAide;
-        
+
         JButton AccueilBtnSinscrire = new JButton("INSCRIPTION");
         AccueilBtnSinscrire.setBackground(new Color(255, 228, 196));
         AccueilBtnSinscrire.setFont(new Font("Tahoma", Font.BOLD, 11));
-        
+
         JButton AccueilBtnSeConnecterTemp = new JButton("LOCATION TEMPORAIRE");
-        AccueilBtnSeConnecterTemp.setBackground(new Color(22,184,78));
+        AccueilBtnSeConnecterTemp.setBackground(new Color(22, 184, 78));
         AccueilBtnSeConnecterTemp.setFont(new Font("Tahoma", Font.BOLD, 13));
-        
+
         JButton AccueilBtnSeConnecter = new JButton("CONNEXION");
-        AccueilBtnSeConnecter.setBackground(new Color(22,184,78));
+        AccueilBtnSeConnecter.setBackground(new Color(22, 184, 78));
         AccueilBtnSeConnecter.setFont(new Font("Tahoma", Font.BOLD, 13));
-        
+
         JButton AccueilBtnAfficherCarte = new JButton("Carte bornes");
-        
+
         JButton btnContact = new JButton("Contact");
-        
+
         JSeparator separator = new JSeparator();
-        
-        JLabel lblVousLouezRgulirement = new JLabel("Vous louez r\u00E9guli\u00E8rement et n'\u00EAtes pas encore inscrit ?");
+
+        JLabel lblVousLouezRgulirement = new JLabel(
+                "Vous louez r\u00E9guli\u00E8rement et n'\u00EAtes pas encore inscrit ?");
         lblVousLouezRgulirement.setFont(new Font("Tahoma", Font.ITALIC, 13));
-        
+
         JLabel lblVelocation = new JLabel("Velocation");
         lblVelocation.setFont(new Font("Tahoma", Font.ITALIC, 16));
-        
+
         GroupLayout groupLayout = new GroupLayout(frmAccueil.getContentPane());
-        groupLayout.setHorizontalGroup(
-        	groupLayout.createParallelGroup(Alignment.TRAILING)
-        		.addGroup(groupLayout.createSequentialGroup()
-        			.addGap(103)
-        			.addComponent(AccueilBtnAide, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
-        			.addGap(32)
-        			.addComponent(AccueilBtnAfficherCarte, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)
-        			.addGap(34)
-        			.addComponent(btnContact, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-        			.addGap(111))
-        		.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-        			.addGap(81)
-        			.addComponent(AccueilBtnSeConnecterTemp, GroupLayout.PREFERRED_SIZE, 205, GroupLayout.PREFERRED_SIZE)
-        			.addGap(18)
-        			.addComponent(AccueilBtnSeConnecter, GroupLayout.PREFERRED_SIZE, 204, GroupLayout.PREFERRED_SIZE)
-        			.addContainerGap(86, Short.MAX_VALUE))
-        		.addComponent(separator, GroupLayout.DEFAULT_SIZE, 594, Short.MAX_VALUE)
-        		.addGroup(groupLayout.createSequentialGroup()
-        			.addGap(210)
-        			.addComponent(AccueilBtnSinscrire, GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE)
-        			.addContainerGap(217, Short.MAX_VALUE))
-        		.addGroup(groupLayout.createSequentialGroup()
-        			.addGap(132)
-        			.addComponent(lblVousLouezRgulirement, GroupLayout.PREFERRED_SIZE, 337, GroupLayout.PREFERRED_SIZE)
-        			.addContainerGap(125, Short.MAX_VALUE))
-        		.addGroup(groupLayout.createSequentialGroup()
-        			.addContainerGap()
-        			.addComponent(lblVelocation, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
-        			.addGap(141)
-        			.addComponent(AccueilLblVelocation, GroupLayout.DEFAULT_SIZE, 155, Short.MAX_VALUE)
-        			.addGap(202))
-        		.addGroup(groupLayout.createSequentialGroup()
-        			.addContainerGap(183, Short.MAX_VALUE)
-        			.addComponent(AccueilLblBienvenue)
-        			.addGap(171))
-        		.addGroup(groupLayout.createSequentialGroup()
-        			.addContainerGap(211, Short.MAX_VALUE)
-        			.addComponent(AccueilLblBorne)
-        			.addGap(191))
-        );
-        groupLayout.setVerticalGroup(
-        	groupLayout.createParallelGroup(Alignment.TRAILING)
-        		.addGroup(groupLayout.createSequentialGroup()
-        			.addContainerGap()
-        			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-        				.addComponent(AccueilLblVelocation, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
-        				.addComponent(lblVelocation))
-        			.addPreferredGap(ComponentPlacement.RELATED)
-        			.addComponent(AccueilLblBorne)
-        			.addPreferredGap(ComponentPlacement.RELATED)
-        			.addComponent(AccueilLblBienvenue)
-        			.addGap(46)
-        			.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-        				.addComponent(AccueilBtnSeConnecter, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
-        				.addComponent(AccueilBtnSeConnecterTemp, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE))
-        			.addGap(38)
-        			.addComponent(lblVousLouezRgulirement, GroupLayout.DEFAULT_SIZE, 20, Short.MAX_VALUE)
-        			.addPreferredGap(ComponentPlacement.UNRELATED)
-        			.addComponent(AccueilBtnSinscrire, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
-        			.addGap(34)
-        			.addComponent(separator, GroupLayout.PREFERRED_SIZE, 13, GroupLayout.PREFERRED_SIZE)
-        			.addGap(8)
-        			.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-        				.addComponent(AccueilBtnAide, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
-        				.addComponent(AccueilBtnAfficherCarte, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
-        				.addComponent(btnContact, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE))
-        			.addContainerGap())
-        );
+        groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.TRAILING).addGroup(groupLayout
+                .createSequentialGroup().addGap(103)
+                .addComponent(AccueilBtnAide, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE).addGap(32)
+                .addComponent(AccueilBtnAfficherCarte, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)
+                .addGap(34).addComponent(btnContact, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
+                .addGap(111))
+                .addGroup(Alignment.LEADING,
+                        groupLayout.createSequentialGroup().addGap(81)
+                                .addComponent(AccueilBtnSeConnecterTemp, GroupLayout.PREFERRED_SIZE, 205,
+                                        GroupLayout.PREFERRED_SIZE)
+                                .addGap(18)
+                                .addComponent(AccueilBtnSeConnecter, GroupLayout.PREFERRED_SIZE, 204,
+                                        GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(86, Short.MAX_VALUE))
+                .addComponent(separator, GroupLayout.DEFAULT_SIZE, 594, Short.MAX_VALUE)
+                .addGroup(
+                        groupLayout.createSequentialGroup().addGap(210)
+                                .addComponent(AccueilBtnSinscrire, GroupLayout.PREFERRED_SIZE, 167,
+                                        GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(217, Short.MAX_VALUE))
+                .addGroup(groupLayout.createSequentialGroup().addGap(132)
+                        .addComponent(lblVousLouezRgulirement, GroupLayout.PREFERRED_SIZE, 337,
+                                GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(125, Short.MAX_VALUE))
+                .addGroup(groupLayout.createSequentialGroup().addContainerGap()
+                        .addComponent(lblVelocation, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
+                        .addGap(141).addComponent(AccueilLblVelocation, GroupLayout.DEFAULT_SIZE, 155, Short.MAX_VALUE)
+                        .addGap(202))
+                .addGroup(groupLayout.createSequentialGroup().addContainerGap(183, Short.MAX_VALUE)
+                        .addComponent(AccueilLblBienvenue).addGap(171))
+                .addGroup(groupLayout.createSequentialGroup().addContainerGap(211, Short.MAX_VALUE)
+                        .addComponent(AccueilLblBorne).addGap(191)));
+        groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+                .addGroup(groupLayout.createSequentialGroup().addContainerGap()
+                        .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+                                .addComponent(AccueilLblVelocation, GroupLayout.PREFERRED_SIZE, 50,
+                                        GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblVelocation))
+                        .addPreferredGap(ComponentPlacement.RELATED).addComponent(AccueilLblBorne)
+                        .addPreferredGap(ComponentPlacement.RELATED).addComponent(AccueilLblBienvenue).addGap(46)
+                        .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+                                .addComponent(AccueilBtnSeConnecter, GroupLayout.PREFERRED_SIZE, 95,
+                                        GroupLayout.PREFERRED_SIZE)
+                                .addComponent(AccueilBtnSeConnecterTemp, GroupLayout.PREFERRED_SIZE, 94,
+                                        GroupLayout.PREFERRED_SIZE))
+                        .addGap(38).addComponent(lblVousLouezRgulirement, GroupLayout.DEFAULT_SIZE, 20, Short.MAX_VALUE)
+                        .addPreferredGap(ComponentPlacement.UNRELATED)
+                        .addComponent(AccueilBtnSinscrire, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
+                        .addGap(34).addComponent(separator, GroupLayout.PREFERRED_SIZE, 13, GroupLayout.PREFERRED_SIZE)
+                        .addGap(8)
+                        .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+                                .addComponent(AccueilBtnAide, GroupLayout.PREFERRED_SIZE, 32,
+                                        GroupLayout.PREFERRED_SIZE)
+                                .addComponent(AccueilBtnAfficherCarte, GroupLayout.PREFERRED_SIZE, 32,
+                                        GroupLayout.PREFERRED_SIZE)
+                                .addComponent(btnContact, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap()));
         frmAccueil.getContentPane().setLayout(groupLayout);
 
         // ActionListener
